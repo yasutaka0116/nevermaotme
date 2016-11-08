b14b20dba4aaead383ab8ab3517e0dbb8ba5d966
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-11-08 14:36:50 +0900

    ルート設定　日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 root 'matomes#index'
+
+resources :matomes, only:[:new, :create, :show]
+resources :users, only: [:edit, :update]
 
 end
